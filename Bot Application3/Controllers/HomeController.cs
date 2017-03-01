7af6d48815a0b0a02cd986baa97cf218eab0f330
LiMeng Liu <v-limliu@microsoft.com>
2017-03-01 13:50:21 +0800

    add webchat

--- Bot Application3/Controllers/HomeController.cs
+++ Bot Application3/Controllers/HomeController.cs
@@ -1,12 +1,15 @@
-﻿using System.Web.Mvc;
+﻿using System.Threading.Tasks;
+using System.Web.Mvc;
+using Bot_Application3.model;
 
 namespace Bot.Controllers
 {
     public class HomeController : Controller
     {
         // GET: Default
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
+            ViewData.Model = (await WebChatToken.GetToken(Session.SessionID)).Token;
             return View();
         }
     }
---
+++ Bot Application3/model/WebChatToken.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Timers;
+using Bot.Utilities;
+namespace Bot_Application3.model
+{
+    class WebChatToken
+    {
+        private Timer timer;
+        private static List<WebChatToken> tokens = new List<WebChatToken>();
+        public string SessionId { get; set; }
+
+        public string Token { get; set; }
+
+        private WebChatToken(string sessionId, string token)
+        {
+            SessionId = sessionId;
+            Token = token;
+            timer = new Timer(60 * 3);
+            timer.Elapsed += Elapsed_Event;
+            tokens.Add(this);
+            timer.Start();
+        }
+
+        public static async Task<WebChatToken> GetToken(string sessionId)
+        {
+            var result = tokens.Where(m => m.SessionId == sessionId).FirstOrDefault();
+            if (result == null)
+            {
+                return new WebChatToken(sessionId, (await BotUtil.GetWebChatToken()).Trim('"'));
+            }
+            return result;
+        }
+
+        private void Elapsed_Event(object sender, ElapsedEventArgs e)
+        {
+            timer.Elapsed -= Elapsed_Event;
+            tokens.Remove(this);
+        }
+    }
+}
